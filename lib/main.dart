5a432f598c6573af04840b01689694eae449c168
Oladapo Omonayajo <o.omonayajo@gmail.com>
2019-12-08 10:42:26 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNumber = 1;
+  int rightDieNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDieNumber = newDieFace();
+      rightDieNumber = newDieFace();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: changeDiceFace,
+              child: Image.asset('images/dice$leftDieNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: changeDiceFace,
+              child: Image.asset('images/dice$rightDieNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  int newDieFace () {
+    return Random().nextInt(5) + 1;
   }
 }
+
